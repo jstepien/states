bf1c67eab61f7e303c2f30cf2ee83545fd0fa30d
Jan Stępień <jstepien@users.sourceforge.net>
2014-10-27 18:11:52 +0100

    Add some missing fields to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject states "0.0.0-SNAPSHOT"
+  :description "Property-based generattive testing for stateful computations"
+  :url "https://github.com/jstepien/states"
+  :license {:name "MIT License"
+            :url "http://git.io/3xfEyA"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.check "0.5.9"]])
