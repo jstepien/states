840c80ebc56ae0e73c8ac24a1aeb66cc26be0e26
Jan Stępień <jstepien@users.sourceforge.net>
2014-10-27 18:08:13 +0100

    Version 0.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject states "0.0.0"
+(defproject states "0.0.1-SNAPSHOT"
   :description "Property-based generattive testing for stateful computations"
   :url "https://github.com/jstepien/states"
   :license {:name "MIT License"
