4ebaecd6c099bb0810deb06ade395666b04edf51
Jan Stępień <jstepien@users.sourceforge.net>
2015-06-07 21:53:19 +0200

    git.io speaks HTTPS

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Property-based generattive testing for stateful computations"
   :url "https://github.com/jstepien/states"
   :license {:name "MIT License"
-            :url "http://git.io/3xfEyA"}
+            :url "https://git.io/3xfEyA"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/test.check "0.5.9"]])
